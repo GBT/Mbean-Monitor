d43edd2b5fb94a2db56fb3ddbef23b39c9898b58
bardouillet <4819671+GBT@users.noreply.github.com>
2011-04-06 13:44:08 +0000

    Source code regeneration, after XSD update

--- Mbean-Monitor/src/main/java/org/sourceforge/mbeanmonitoring/report/castor/descriptors/MbeanDescriptor.java
+++ Mbean-Monitor/src/main/java/org/sourceforge/mbeanmonitoring/report/castor/descriptors/MbeanDescriptor.java
@@ -108,7 +108,8 @@ public class MbeanDescriptor extends org.exolab.castor.xml.util.XMLClassDescript
         }
         desc.setValidator(fieldValidator);
         //-- _domain
-        desc = new org.exolab.castor.xml.util.XMLFieldDescriptorImpl(org.sourceforge.mbeanmonitoring.report.castor.types.MbeanDomainType.class, "_domain", "domain", org.exolab.castor.xml.NodeType.Attribute);
+        desc = new org.exolab.castor.xml.util.XMLFieldDescriptorImpl(java.lang.String.class, "_domain", "domain", org.exolab.castor.xml.NodeType.Attribute);
+        desc.setImmutable(true);
         handler = new org.exolab.castor.xml.XMLFieldHandler() {
             public java.lang.Object getValue( java.lang.Object object ) 
                 throws IllegalStateException
@@ -121,7 +122,7 @@ public class MbeanDescriptor extends org.exolab.castor.xml.util.XMLClassDescript
             {
                 try {
                     Mbean target = (Mbean) object;
-                    target.setDomain( (org.sourceforge.mbeanmonitoring.report.castor.types.MbeanDomainType) value);
+                    target.setDomain( (java.lang.String) value);
                 } catch (java.lang.Exception ex) {
                     throw new IllegalStateException(ex.toString());
                 }
@@ -130,9 +131,7 @@ public class MbeanDescriptor extends org.exolab.castor.xml.util.XMLClassDescript
                 return null;
             }
         };
-        handler = new org.exolab.castor.xml.handlers.EnumFieldHandler(org.sourceforge.mbeanmonitoring.report.castor.types.MbeanDomainType.class, handler);
-        desc.setImmutable(true);
-        desc.setSchemaType("MbeanDomainType");
+        desc.setSchemaType("string");
         desc.setHandler(handler);
         desc.setRequired(true);
         desc.setMultivalued(false);
@@ -142,6 +141,10 @@ public class MbeanDescriptor extends org.exolab.castor.xml.util.XMLClassDescript
         fieldValidator = new org.exolab.castor.xml.FieldValidator();
         fieldValidator.setMinOccurs(1);
         { //-- local scope
+            org.exolab.castor.xml.validators.StringValidator typeValidator;
+            typeValidator = new org.exolab.castor.xml.validators.StringValidator();
+            fieldValidator.setValidator(typeValidator);
+            typeValidator.setWhiteSpace("preserve");
         }
         desc.setValidator(fieldValidator);
         //-- _filename
